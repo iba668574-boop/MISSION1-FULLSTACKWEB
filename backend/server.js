cd62889590587ddca95eac814597fc8d7cb4b4b0
Sorna Abinaya <iba668574@gmail.com>
2026-06-20 20:46:57 +0530

    Mission 1 completed

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,57 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 10000
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("MongoDB Error:", err));
+
 let items = [
-    { id: 1, name: "Laptop" },
-    { id: 2, name: "Mobile" },
-    { id: 3, name: "Keyboard" }
+  { id: 1, name: "Laptop" },
+  { id: 2, name: "Mobile" },
+  { id: 3, name: "Keyboard" }
 ];
 
-/* GET ALL */
+// GET ALL
 app.get("/items", (req, res) => {
-    res.json(items);
+  res.json(items);
 });
 
-/* GET ONE */
+// GET ONE
 app.get("/items/:id", (req, res) => {
+  const item = items.find(x => x.id == req.params.id);
 
-    const item = items.find(
-        x => x.id == req.params.id
-    );
-
-    if(item){
-        res.json(item);
-    } else {
-        res.status(404).json({
-            message: "Item not found"
-        });
-    }
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({
+      message: "Item not found"
+    });
+  }
 });
 
-/* POST CREATE */
+// POST CREATE
 app.post("/items", (req, res) => {
+  const newItem = {
+    id: items.length + 1,
+    name: req.body.name
+  };
 
-    const newItem = {
-        id: items.length + 1,
-        name: req.body.name
-    };
+  items.push(newItem);
 
-    items.push(newItem);
-
-    res.status(201).json(newItem);
+  res.status(201).json(newItem);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
